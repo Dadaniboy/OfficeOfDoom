dba903f7d9f6cea4d579c0d2c760bd9bcfd11c17
Faxinger <cmeyer1101@gmail.com>
2017-07-04 13:26:03 +0200

    Aktualisierung

---
+++ SpielFenster.java
@@ -0,0 +1,20 @@
+import java.awt.*;
+import java.awt.event.*;
+
+public class SpielFenster extends Frame{
+	
+	public SpielFenster (String Titel) {
+		
+		this.setTitle(Titel);
+		this.setSize(640,640);
+				
+		this.addWindowListener(new WindowAdapter() {
+			
+			public void windowClosing(WindowEvent e){
+				System.exit(0);				
+			}
+			
+		});
+	}
+
+}
---
+++ ood.java
@@ -0,0 +1,13 @@
+import java.awt.*;
+
+public class ood {
+
+	public static void main(String[] args) {
+
+		Frame HauptFenster = new SpielFenster("Office of Doom");
+		HauptFenster.setVisible(true);
+
+		
+	}
+
+}
